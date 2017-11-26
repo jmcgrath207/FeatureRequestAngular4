3ce96c12ed73bafa96444b15317a24ac76648d03
john <sonance207@gmail.com>
2017-11-25 20:21:07 -0600

    Updated Ticket Table Model

--- src/app/client-view/ticket-table/ticket-table.model.ts
+++ src/app/client-view/ticket-table/ticket-table.model.ts
@@ -3,26 +3,30 @@
 
 export class TicketTableModel {
   public ticketId: number;
+  public ticketOriginalId: number;
+  public ticketOwnerId: number;
   public title: string;
-  public userId: number;
   public description: string;
   public creationDate: string;
+  public creationUserId: number;
   public updateDate: string;
+  public updateUserId: number;
   public targetDate: string;
   public departmentId: number;
   public statusId: number;
   public priorityId: number;
 
 
-  constructor(ticketId: number, title: string, userId: number,
-              description: string, creationDate: string, updateDate: string,
-              targetDate: string, departmentId: number, statusId: number, priorityId: number) {
+  constructor(ticketId: number, ticketOriginalId: number, ticketOwnerId: number, title: string, description: string, creationDate: string, creationUserId: number, updateDate: string, updateUserId: number, targetDate: string, departmentId: number, statusId: number, priorityId: number) {
     this.ticketId = ticketId;
+    this.ticketOriginalId = ticketOriginalId;
+    this.ticketOwnerId = ticketOwnerId;
     this.title = title;
-    this.userId = userId;
     this.description = description;
     this.creationDate = creationDate;
+    this.creationUserId = creationUserId;
     this.updateDate = updateDate;
+    this.updateUserId = updateUserId;
     this.targetDate = targetDate;
     this.departmentId = departmentId;
     this.statusId = statusId;
