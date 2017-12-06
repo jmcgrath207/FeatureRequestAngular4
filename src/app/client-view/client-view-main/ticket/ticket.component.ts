96bf8e857f5e73f483345c16a96f1591c44b792c
john <sonance207@gmail.com>
2017-12-05 23:53:00 -0600

    Working Ticket Information

--- src/app/client-view/client-view-main/ticket/ticket.component.ts
+++ src/app/client-view/client-view-main/ticket/ticket.component.ts
@@ -3,6 +3,7 @@ import {Store} from "@ngrx/store";
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {ActivatedRoute, Params, Router} from "@angular/router";
 import * as ClientViewReducers from "../../store/client-view.reducers";
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'app-ticket',
@@ -13,37 +14,70 @@ export class TicketComponent implements OnInit {
 
   arrayPosition: number;
   ticketForm: FormGroup;
+  infoMode: boolean;
+  editMode: boolean;
+  addMode: boolean;
+  clientViewState: Observable<ClientViewReducers.State>;
 
 
   constructor(private route: ActivatedRoute,
               private router: Router,
               public fb: FormBuilder,
-              private store: Store<ClientViewReducers.FeatureState>) { }
+              private store: Store<ClientViewReducers.FeatureState>) {
+
+              this.infoMode = false;
+              this.editMode = false;
+              this.addMode = false;
+  }
 
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
-        if (params.type == 'info') {
+
+
           this.arrayPosition = params.arrayPosition;
-          this.initForm();
-        }
+          this.initTemplate(params);
+
 
       }
     );
   }
 
 
-  private initForm() {
+  private initTemplate(params: Params) {
+    if (params.type == 'info') {
+      this.infoMode = true;
+      this.clientViewState = this.store.select('clientview');
+
+    }
+
+
+
+    if (params.type == 'edit') {
+      this.editMode = true;
       this.store.select('clientview')
         .take(1)
         .subscribe((clientViewState: ClientViewReducers.State) => {
           const ticketTable = clientViewState.ticketTable[this.arrayPosition];
           this.ticketForm = this.fb.group({
             ticketTitle: [ticketTable.ticketTitle, Validators.required]
-            })
-      });
+          })
+        });
 
+    }
+    if (params.type == 'add') {
+      this.editMode = true;
+      this.store.select('clientview')
+        .take(1)
+        .subscribe((clientViewState: ClientViewReducers.State) => {
+          const ticketTable = clientViewState.ticketTable[this.arrayPosition];
+          this.ticketForm = this.fb.group({
+            ticketTitle: ["", Validators.required]
+          })
+        });
+
+    }
   }
 
 }
