c98b872ce4defedb4493a7c4052f5fdc4ceb3a03
john <sonance207@gmail.com>
2017-12-29 20:00:31 -0600

    Diffs working for historical comments

--- src/app/client-view/client-view-main/ticket-comments/ticket-comments.component.ts
+++ src/app/client-view/client-view-main/ticket-comments/ticket-comments.component.ts
@@ -6,6 +6,8 @@ import {selectCommentHistoryTable, selectCommentTable} from "../../store/client-
 import {FetchCommentHistoryTable, FetchCommentsTable} from "../../store/client-view.actions";
 import "rxjs/add/operator/takeWhile";
 import {CommentsTableModel} from "./comments-table.model";
+import {default as dDiff} from 'deep-diff'
+import {forEach} from "@angular/router/src/utils/collection";
 
 
 @Component({
@@ -24,6 +26,7 @@ export class TicketCommentsComponent implements OnInit {
 
 
 
+
   constructor(private route: ActivatedRoute,
               private store: Store<FeatureState>) {}
 
@@ -37,7 +40,7 @@ export class TicketCommentsComponent implements OnInit {
           (data: any) => {
             if (Object.keys(data[0]).length !== 0) {
               let commmentNumber = data.length;
-              data.forEach(function (element) {
+              data.forEach(element => {
                 element["commentNumber"] = commmentNumber;
                 element["showHistory"] = false;
                 commmentNumber = commmentNumber - 1;
@@ -53,21 +56,37 @@ export class TicketCommentsComponent implements OnInit {
     );
   }
 
-  toggleCommentHistory(index: number) {
+
+
+
+
+
+
+
+
+
+   toggleCommentHistory(index: number) {
     if(this.newCommentTable[index]["showHistory"] == false){
       this.store.dispatch(new FetchCommentHistoryTable(this.newCommentTable[index]["commentOriginalId"]));
       this.store.select(selectCommentHistoryTable).take(2).subscribe(
-        (data: any) => {
-          if (Object.keys(data[0]).length !== 0) {
-            let commmentNumber = data.length;
-            data.forEach(function (element) {
+        (CommentHistoryData: any) => {
+          if (Object.keys(CommentHistoryData[0]).length !== 0) {
+            let commmentNumber = CommentHistoryData.length;
+            CommentHistoryData.forEach(element => {
+
+
+
+              element = this.commentdiff(CommentHistoryData,element,index);
+
+
+
               element["commentNumber"] = commmentNumber;
               commmentNumber = commmentNumber - 1;
 
             });
             this.newCommentHistoryObject[index] = {
-              'CommentHistory' : data,
-              'CommentHistoryLength': data.length
+              'CommentHistory' : CommentHistoryData,
+              'CommentHistoryLength': CommentHistoryData.length
             };
           }
         }
@@ -80,6 +99,42 @@ export class TicketCommentsComponent implements OnInit {
     }
 
   }
+
+
+
+
+  private commentdiff(CommentHistoryData: any, element: CommentsTableModel, index: number) {
+
+    let newObject = {};
+
+    let dataIndexOf = CommentHistoryData.indexOf(element);
+
+    //find difference between historical comment and current
+    if (dataIndexOf + 1 == CommentHistoryData.length){
+      let leftHandedSide = this.newCommentTable[index]; //old
+      let rightHandedSide = element; //new
+
+
+
+    }
+
+    //find difference between historical comments
+    let leftHandedSide = element;
+    let rightHandedSide = CommentHistoryData[CommentHistoryData.indexOf(element)+ 1]; //old
+    let differences = dDiff.diff(leftHandedSide, rightHandedSide); // new
+    differences.forEach( diff => {
+      let changedKey = diff.path[0];
+      if(diff.kind == "E" && changedKey !="commentId") {
+        newObject[changedKey] = diff.lhs;
+        console.log("test")
+        }
+    });
+
+    return newObject
+  }
+
+
+
 
 
 
